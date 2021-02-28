4c23f5a6585fec9f612fd95790ec5a91dfd7ef98
Pedro <pedro@ezoom.com.br>
2021-02-28 14:54:15 -0300

    [aula 05] - Modulos do angular (ngModule)

---
+++ primeiro-projeto/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-curso-detalhe',
+    templateUrl: './curso-detalhe.component.html',
+    styleUrls: ['./curso-detalhe.component.css']
+})
+
+export class CursoDetalheComponent {}
