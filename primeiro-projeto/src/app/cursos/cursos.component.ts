e84459a293e5f584ca2d45d1305622739d15a238
Pedro <pedro@ezoom.com.br>
2021-02-28 15:23:07 -0300

    [aula 06] - Introduçao ao templates

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+// O template inline só deve ser utilizado até 3 linhas
+// Para utilizá-lo dentro dos metadados use template: `dados do template` , utilizando template literals
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos = {
+    'curso_um': 'Java',
+    'curso_dois': 'Angular'
+  }
+  alunos: string[] = [
+    'Pedro Lima',
+    'Mayara Lima',
+    'Nicolas Lima'
+  ]
+
+  constructor() {
+    this.nomePortal = 'http://loaine.training'
+  }
 
   ngOnInit(): void {
   }
